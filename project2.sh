26854fb9f90b5ad037a62c667835fee17601e6a3
Dent-taha <tashraf853@gmail.com>
2025-07-05 00:16:53 +0300

    improvmet project2 (2)

--- project2.sh
+++ project2.sh
@@ -3,7 +3,7 @@
 backup_file="backup.log"
 backup_dir="/home/user/backup"
 time=$(date +"%d-%m-%Y-%H%M")
-target="$1"
+target="$2"
 
 
 
@@ -16,51 +16,54 @@ fi
 
 archiving()
 {
-	tar -czvf  "$target".tar.gz."$time" "$target"
-	mv "$target".tar.gz."$time" "$backup_dir"
-	echo " archiving of $target has done in $time" >> "$backup_file"
+	if [[ -z "$target" ]] ;
+	then
+		read -p "type a file or folder you want to archize"  target
+	fi
+	if [[ -d "$target" || -f "$target" ]] ;
+	then
+		archive_name=$(basename "$target")-$time.tar.gz
+
+	tar -czvf  "$archive_name"  "$target"
+	mv "$archive_name " "$backup_dir"
+	echo " archiving of $target has done in time  $time" >> "$backup_file"
+	echo " backup created: $backup_dir/$archive_name"
+else
+	echo "folder or file doesn't exist"
+	exit 1
+	fi
+
 }
 restoring()
 {
-	for restored_file in "$backup_dir"/* ;
-	do
-		if [[ "$2" -eq "$restored_file" ]];
-		then
-			tar -xzf "$2"
-		fi
-	done
+	ls "$backup_di"
+	read -p "type the file you want to restor: " file
+	full_path="$backup_dir/$file"
+	if [[ -f "$full_path" ]];
+	then
+		tar xzf "$full_path"
+		echo "restored: $file"
+	else
+		echo : file not fount
+	fi
 }
 
 listing()
 {
-	for files in "$backup_dir"/* ;
-	do
-		echo "$files"
-	done
+	echo "files  in  $backup_dir"
+	ls "$backup_die"
 }
 
 
 case $1 in 
-	--backup ) shift ; archiving
+	--backup )  archiving
 		;;
 
 	--restore ) restoring
 		;;
 	--list ) listing
 		;;
-	* ) echo " Usage : [ --restoring | --listing ] "
+	* ) echo " Usage : [ --restoring | --listing |--backup] "
 esac
 
-if [[ $# != 1 ]] ;
-then
-	read -p "type a folder you want to archive it : " file
-	if [[ -f "$file" ]] ;
-	then
-		target="$file"
-		archiving
-	else
-		echo "file not exist"
-		exit 1
-	fi
-fi
 
